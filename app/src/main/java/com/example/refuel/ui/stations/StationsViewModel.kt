5f981d1b35d831f0650509b6e21f2dfd8bcb8a16
alengozy <alen1199@gmail.com>
2020-10-01 16:50:23 +0600

    Added russian locale file, adjusted fragments

--- app/src/main/java/com/example/refuel/ui/stations/StationsViewModel.kt
+++ app/src/main/java/com/example/refuel/ui/stations/StationsViewModel.kt
@@ -1,10 +1,10 @@
-package com.example.refuel.ui.dashboard
+package com.example.refuel.ui.stations
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class DashboardViewModel : ViewModel() {
+class StationsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is dashboard Fragment"
--- app/src/main/java/com/example/refuel/ui/user/UserFragment.kt
+++ app/src/main/java/com/example/refuel/ui/user/UserFragment.kt
@@ -1,4 +1,4 @@
-package com.example.refuel.ui.notifications
+package com.example.refuel.ui.user
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -6,23 +6,21 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import com.example.refuel.R
 
-class NotificationsFragment : Fragment() {
+class UserFragment : Fragment() {
 
-    private lateinit var notificationsViewModel: NotificationsViewModel
+    private val notificationsViewModel: UserViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        notificationsViewModel =
-            ViewModelProviders.of(this).get(NotificationsViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_notifications, container, false)
-        val textView: TextView = root.findViewById(R.id.text_notifications)
+        val root = inflater.inflate(R.layout.fragment_user, container, false)
+        val textView: TextView = root.findViewById(R.id.text_userfrag)
         notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
